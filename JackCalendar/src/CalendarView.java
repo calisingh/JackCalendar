62f8b33d7cd9cfdf76898102455bc1882ca33709
Jooyul Yoon <55604309+jooyul-yoon@users.noreply.github.com>
2021-07-21 10:32:44 -0700

    Dialogs of Create Button , Load File Button (#3)
    * Left Panel. all buttons update current date
    
    * Added Data Classes: MyCalendar.java, Event.java, TimeInterval.java
    
    * Added Data Classes: MyCalendar.java, Event.java, TimeInterval.java
    
    * Connected Model and View - title is shown in text area based on current date
    
    * Dialogs of Create Button , Load File Button

--- JackCalendar/src/CalendarView.java
+++ JackCalendar/src/CalendarView.java
@@ -3,33 +3,54 @@ import javax.swing.border.EmptyBorder;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import java.time.LocalDate;
 
 import javax.swing.event.*;
+
+/**
+ * View and Controller for Calendar
+ * @author Carissa, Jooyul, Kunwarpreet
+ */
 public class CalendarView implements ChangeListener{
+
+  final int WINDOW_WIDTH = 800;
+  final int WINDOW_HEIGHT = 300;
+  final int BASE_SPACE = 20;
+  final int PANEL_WIDTH = (WINDOW_WIDTH - 3 * BASE_SPACE) / 2;
+  final int PANEL_HEIGHT = WINDOW_HEIGHT - 2 * BASE_SPACE;
+  final int BTN_SIZE = 25;
+  final int CALENDAR_ROW = 6;
+  final int CALENDAR_COL = 7;
+
+  private JFrame frame= new JFrame("JACK CALENDAR"); 
+  private LocalDate currentDate;
+  private CalendarModel model;
+  // private MyCalendar myCalendar;
+
   /* Constructor */
   public CalendarView(CalendarModel m) 
   {
     model = m;
-    myCalendar = m.getCalendar();
+    // myCalendar = m.getCalendar();
     currentDate = LocalDate.now();
-    JFrame f= new JFrame("JACK CALENDAR");    
+       
 
     JPanel leftPanel=new JPanel();   
-    leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
-    leftPanel.setBounds(BASE_SPACE,BASE_SPACE,PANEL_WIDTH,PANEL_HEIGHT);    
+      leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+      leftPanel.setBounds(BASE_SPACE,BASE_SPACE,PANEL_WIDTH,PANEL_HEIGHT);    
     JPanel rightPanel = new JPanel();  
-    rightPanel.setBounds(PANEL_WIDTH + 2 * BASE_SPACE,BASE_SPACE,PANEL_WIDTH,PANEL_HEIGHT);   
+      rightPanel.setBounds(PANEL_WIDTH + 2 * BASE_SPACE,BASE_SPACE,PANEL_WIDTH,PANEL_HEIGHT);   
 
     /* Left Panel  */
     JPanel titlePanel=new JPanel();
-    titlePanel.setLayout(new GridLayout());
-    titlePanel.setBorder(new EmptyBorder(BASE_SPACE /  2, BASE_SPACE, BASE_SPACE /  2, BASE_SPACE));
+      titlePanel.setLayout(new GridLayout());
+      titlePanel.setBorder(new EmptyBorder(BASE_SPACE /  2, BASE_SPACE, BASE_SPACE /  2, BASE_SPACE));
     JPanel calendarPanel=new JPanel();
     
     /* Left Panel Component */
     JLabel monthLabel = new JLabel(getMonthAbbreviation(currentDate), SwingConstants.CENTER);
-    monthLabel.setFont(new Font("Arial", Font.BOLD, 20));
+      monthLabel.setFont(new Font("Arial", Font.BOLD, 20));
     JLabel yearLabel = new JLabel(Integer.toString(currentDate.getYear()));
     JButton btnPrevMonth=new JButton("<"); btnPrevMonth.setPreferredSize(new Dimension(BTN_SIZE, BTN_SIZE));
     JButton btnNextMonth=new JButton(">"); btnNextMonth.setPreferredSize(new Dimension(BTN_SIZE, BTN_SIZE));
@@ -42,12 +63,12 @@ public class CalendarView implements ChangeListener{
     JButton btnA = new JButton("Agenda");
     JTextArea content = new JTextArea(11, 30);
     JButton btnCreate = new JButton("Create");
-    JButton btnEdit = new JButton("Edit");
-    JButton btnDelete = new JButton("Delete");
-    JButton btnLoadFile = new JButton("File");
+    // JButton btnEdit = new JButton("Edit");
+    // JButton btnDelete = new JButton("Delete");
+    JButton btnLoadFile = new JButton("From File");
 
     /* Initialize Calendar */
-    calendarDateBtnHandler(calendarPanel, currentDate, content);
+    showMonthlyCalendar(calendarPanel, currentDate, content);
 
     content.setText("Welcome to JACK Calendar!");
     /* ActionListeners */
@@ -60,7 +81,7 @@ public class CalendarView implements ChangeListener{
         yearLabel.setText(Integer.toString(currentDate.getYear()));
         // update monthly calendar
         calendarPanel.removeAll();
-        calendarDateBtnHandler(calendarPanel, currentDate, content);
+        showMonthlyCalendar(calendarPanel, currentDate, content);
       }
     });
     btnNextMonth.addActionListener(new ActionListener() {
@@ -72,7 +93,7 @@ public class CalendarView implements ChangeListener{
         yearLabel.setText(Integer.toString(currentDate.getYear()));
         // update monthly calendar
         calendarPanel.removeAll();
-        calendarDateBtnHandler(calendarPanel, currentDate, content);
+        showMonthlyCalendar(calendarPanel, currentDate, content);
       }
     });
     btnToday.addActionListener(new ActionListener() {
@@ -84,7 +105,17 @@ public class CalendarView implements ChangeListener{
         yearLabel.setText(Integer.toString(currentDate.getYear()));
         // update monthly calendar
         calendarPanel.removeAll();
-        calendarDateBtnHandler(calendarPanel, currentDate, content);
+        showMonthlyCalendar(calendarPanel, currentDate, content);
+      }
+    });
+    btnCreate.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        createEventDialog();
+      }
+    });
+    btnLoadFile.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        loadFileDialog();
       }
     });
 
@@ -103,38 +134,22 @@ public class CalendarView implements ChangeListener{
     rightPanel.add(btnA);
     rightPanel.add(content);
     rightPanel.add(btnCreate);
-    rightPanel.add(btnEdit);
-    rightPanel.add(btnDelete);
+    // rightPanel.add(btnEdit);
+    // rightPanel.add(btnDelete);
     rightPanel.add(btnLoadFile);
     
-    f.add(leftPanel); f.add(rightPanel);  
+    frame.add(leftPanel); frame.add(rightPanel);  
     
-    f.setSize(WINDOW_WIDTH, WINDOW_HEIGHT + 28);    
-    f.setLayout(null);    
-    f.setVisible(true);
+    frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT + 28);    
+    frame.setLayout(null);    
+    frame.setVisible(true);
   }
 
-  /**
-   * January	Jan.
-   * February	Feb.
-   * March	Mar.
-   * April	Apr.
-   * May	May
-   * June	Jun.
-   * July	Jul.
-   * August	Aug.
-   * September	Sep. or Sept.
-   * October	Oct.
-   * December	Dec.
-   * November	Nov.
-   * @param date Local Date
-   * @return String of Month Abbreviations
-   */
-  public String getMonthAbbreviation(LocalDate date) { 
-    return date.getMonth().toString().substring(0, 3);
+  @Override
+  public void stateChanged(ChangeEvent e) {
   }
 
-  public void calendarDateBtnHandler(JPanel calendarPanel, LocalDate c, JTextArea content) {
+  public void showMonthlyCalendar(JPanel calendarPanel, LocalDate c, JTextArea content) {
     int totalDaysOfMonth = c.getMonth().length(c.isLeapYear());
 		int offset = 0;
 		String firstDayOfMonth = LocalDate.of(c.getYear(), c.getMonth(), 1).getDayOfWeek().name();
@@ -193,8 +208,7 @@ public class CalendarView implements ChangeListener{
           System.out.println("current date is: " + currentDate.toString());
 
           // update content of the date
-          //content.removeAll();
-          content.setText(myCalendar.getEventInfo(currentDate));
+          // content.setText(myCalendar.getEventInfo(currentDate));
         }
       });
     }
@@ -207,23 +221,116 @@ public class CalendarView implements ChangeListener{
       calendarPanel.add(dateBtn);
     }
   }
-  
-  @Override
-  public void stateChanged(ChangeEvent e) {
-    // TODO Auto-generated method stub
+
+  private void createEventDialog() {
+		JDialog eventDialog = new JDialog();
+    JPanel panel = (JPanel) eventDialog.getContentPane();
+		JTextField eventText = new JTextField(10);
+    JTextField yearText = new JTextField(5);
+		JTextField dateText = new JTextField(5);
+		JTextField startTimeText = new JTextField(5);
+		JTextField endTimeText = new JTextField(5);
+		JButton btnSave = new JButton("Save");
+
+    btnSave.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        // Empty input
+        if(
+          eventText.getText().isEmpty() || 
+          yearText.getText().isEmpty() || 
+          dateText.getText().isEmpty() || 
+          startTimeText.getText().isEmpty() || 
+          endTimeText.getText().isEmpty()
+        ) {
+          JDialog ErrorDialog = new JDialog();
+          JButton btnClose = new JButton("Close");
+          btnClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              ErrorDialog.dispose();
+            }
+          });
+          ErrorDialog.setLocation(100, 100);
+          ErrorDialog.setSize(new Dimension(100, 100));
+          ErrorDialog.add(new JLabel("Error: Empty Input"));
+          ErrorDialog.add(btnClose);
+          ErrorDialog.setLayout(new GridLayout(2, 0));
+          ErrorDialog.pack();
+          ErrorDialog.setVisible(true);
+        }
+        else {
+          String eventTitle = eventText.getText();
+          String eventSchedule = yearText.getText() + ";" + dateText.getText()    + ";" + 
+                                  startTimeText.getText() + ";" + endTimeText.getText() + ";";
+          // Event newEvent = new Event(eventTitle, eventSchedule);
+        }
+
+        // close dialog
+        eventDialog.dispose();
+      }
+    });
+
+    panel.add(new JLabel("Event Name"));
+    panel.add(eventText);
+    panel.add(new JLabel("Year"));
+    panel.add(yearText);
+    panel.add(new JLabel("Date"));
+    panel.add(dateText);
+    panel.add(new JLabel("Start Time (0~23)"));
+    panel.add(startTimeText);
+    panel.add(new JLabel("End Time (0~23)"));
+    panel.add(endTimeText);
+    panel.add(new JLabel(""));
+    panel.add(btnSave);
     
+    panel.setBorder(new EmptyBorder(5, 10, 5, 10));
+		eventDialog.setTitle("Create event");
+		eventDialog.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+    eventDialog.setLayout(new GridLayout(6, 2));
+		eventDialog.pack();
+		eventDialog.setVisible(true);
+  }
+
+  public void loadFileDialog(){
+    JFrame frame = new JFrame("File Browser");
+
+    JFileChooser fileChooser = new JFileChooser(".");
+    JButton btnClose = new JButton("Close");
+    fileChooser.setControlButtonsAreShown(false);
+    frame.getContentPane().add(fileChooser, BorderLayout.CENTER);
+    frame.getContentPane().add(btnClose, BorderLayout.SOUTH);
+    btnClose.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        frame.dispose();
+      }
+    });
+    fileChooser.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent actionEvent) {
+          JFileChooser theFileChooser = (JFileChooser) actionEvent
+                  .getSource();
+          String command = actionEvent.getActionCommand();
+          if (command.equals(JFileChooser.APPROVE_SELECTION)) {
+              File selectedFile = theFileChooser.getSelectedFile();
+              // Handle Loaded File Here
+              System.out.println(selectedFile.getParent());
+              System.out.println(selectedFile.getName());
+          } else if (command.equals(JFileChooser.CANCEL_SELECTION)) {
+              System.out.println(JFileChooser.CANCEL_SELECTION);
+          }
+    }});
+    frame.pack();
+    frame.setVisible(true);
   }
   
-  final int WINDOW_WIDTH = 800;
-  final int WINDOW_HEIGHT = 300;
-  final int BASE_SPACE = 20;
-  final int PANEL_WIDTH = (WINDOW_WIDTH - 3 * BASE_SPACE) / 2;
-  final int PANEL_HEIGHT = WINDOW_HEIGHT - 2 * BASE_SPACE;
-  final int BTN_SIZE = 25;
-  final int CALENDAR_ROW = 6;
-  final int CALENDAR_COL = 7;
-
-  private LocalDate currentDate;
-  private MyCalendar myCalendar;
-  private CalendarModel model;
+  /**
+   * January Jan. / February	Feb. / March	Mar. / April	Apr. /
+   * May	May / June	Jun. / July	Jul. / August	Aug. /
+   * September Sep. / October	Oct. / November	Nov. / December	Dec.
+   * @param date Local Date
+   * @return String of Month Abbreviations
+   */
+  public String getMonthAbbreviation(LocalDate date) { 
+    return date.getMonth().toString().substring(0, 3);
+  }
+  
 }
