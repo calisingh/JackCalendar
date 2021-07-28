fb5c93f809ac16ec44ff4479f774fa0b7bc17634
Jooyul Yoon <juyeolyoon@gmail.com>
2021-07-28 00:44:03 -0700

    test case added

--- JackCalendar/src/MyCalendar.java
+++ JackCalendar/src/MyCalendar.java
@@ -57,45 +57,71 @@ public class MyCalendar {
 	 * @param filename file File: file to read reservations from
 	 * @throws FileNotFoundException
 	 */
-	public void loadAndUpdateEvents(String filename) throws FileNotFoundException{
+	public void loadAndUpdateEvents(String filename) throws FileNotFoundException {
 		print("## Loading '" + filename +"' ##");
 		File file = new File(filename);
 		Scanner fileScanner = new Scanner(file);
 		while (fileScanner.hasNextLine()) {
-			String[] details = fileScanner.nextLine().split(";");
-			String name = details[0];
-			int year = Integer.parseInt(details[1]);
-			int startMonth = Integer.parseInt(details[2]);
-			int endMonth = Integer.parseInt(details[3]);
-			String recursOn = details[4];
-			LocalDate startDate = LocalDate.of(year, startMonth, 1).with(TemporalAdjusters.firstDayOfMonth());
-			LocalDate endDate = LocalDate.of(year, endMonth, 1).with(TemporalAdjusters.lastDayOfMonth());
-			String startTime = details[5];
-			String endTime = details[6];
+			String eventInfo = fileScanner.nextLine();
+			String[] details = eventInfo.split(";");
 
-			if(Integer.parseInt(startTime) < 10) 	startTime = "0" + startTime;
-			if(Integer.parseInt(endTime) < 10) 			endTime = "0" + endTime;
-			startTime += ":00";
-			endTime += ":00";
-
-			List<LocalDate> tempLocalDateList = startDate.datesUntil(endDate.plusDays(1))
-																			.collect(Collectors.toList());
-			ArrayList<String> dofList = new ArrayList<String>();
-			for(int i = 0; i < recursOn.length(); i++) {
-				dofList.add(getDayOfWeek("" + recursOn.charAt(i)));
-			}
-
-			for(LocalDate c: tempLocalDateList) {
-				if(dofList.contains(c.getDayOfWeek().toString())) {
-					LocalTime start = LocalTime.parse(startTime, DateTimeFormatter.ISO_LOCAL_TIME);
-					LocalTime end = LocalTime.parse(endTime, DateTimeFormatter.ISO_LOCAL_TIME);
-					Event newEvent = new Event(name, c, start, end);
-					if (!isConflicts(newEvent))
-						updateEvent(c, newEvent);
+			try{
+				/* Invalid number of arguments */
+				if(details.length != 7){
+					print("Can't Load '" + eventInfo + "' - Invalid number of arguments");
+					continue;
 				}
-			}
 
-			print("Loading '" + name + "' is done!");
+				String name = details[0];
+				int year = Integer.parseInt(details[1]);
+				int startMonth = Integer.parseInt(details[2]);
+				int endMonth = Integer.parseInt(details[3]);
+				String recursOn = details[4];
+				String startTime = details[5];
+				String endTime = details[6];
+				int startHour = Integer.parseInt(startTime);
+				int endHour = Integer.parseInt(endTime);
+
+				/* Invalid format of input */
+				if((year < 0)  || 
+					(startMonth < 0) || (startMonth > 12) || 
+					(endMonth < 0) 	 || (endMonth > 12) 	||
+					(startHour < 0)  || (startHour > 23) || 
+					(endHour < 0)    || (endHour> 23)){
+					print("Can't Load '" + eventInfo + "' - Invalid format of input");
+					continue;
+				}
+
+				LocalDate startDate = LocalDate.of(year, startMonth, 1).with(TemporalAdjusters.firstDayOfMonth());
+				LocalDate endDate = LocalDate.of(year, endMonth, 1).with(TemporalAdjusters.lastDayOfMonth());
+
+				if(Integer.parseInt(startTime) < 10) 	startTime = "0" + startTime;
+				if(Integer.parseInt(endTime) < 10) 			endTime = "0" + endTime;
+				startTime += ":00";
+				endTime += ":00";
+	
+				List<LocalDate> tempLocalDateList = startDate.datesUntil(endDate.plusDays(1))
+																				.collect(Collectors.toList());
+				ArrayList<String> dofList = new ArrayList<String>();
+				for(int i = 0; i < recursOn.length(); i++) {
+					dofList.add(getDayOfWeek("" + recursOn.charAt(i)));
+				}
+	
+				for(LocalDate c: tempLocalDateList) {
+					if(dofList.contains(c.getDayOfWeek().toString())) {
+						LocalTime start = LocalTime.parse(startTime, DateTimeFormatter.ISO_LOCAL_TIME);
+						LocalTime end = LocalTime.parse(endTime, DateTimeFormatter.ISO_LOCAL_TIME);
+						Event newEvent = new Event(name, c, start, end);
+						if (!isConflicts(newEvent))
+							updateEvent(c, newEvent);
+					}
+				}
+	
+				print("Loading '" + name + "' is done!");
+			}
+			catch (NumberFormatException nfe) {
+				print("Can't Load '" + details[0] + "' - Number Format Exception");
+			}
 		}
 		fileScanner.close();
 		for (ChangeListener l : listeners) {
